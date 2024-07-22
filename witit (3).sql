bd7e94a358aee55beb7319e5e612a2c87e31f829
Far <farrahmaemagana@gmail.com>
2024-07-22 18:49:49 +0800

    Update witit.sql

--- witit (3).sql
+++ witit (3).sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 22, 2024 at 12:47 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `witit`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `debts`
---
 
 CREATE TABLE `debts` (
   `debt_id` int(11) NOT NULL,
@@ -36,13 +15,8 @@ CREATE TABLE `debts` (
   `amount` decimal(10,2) NOT NULL,
   `remaining_balance` decimal(10,2) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+)
 
--- --------------------------------------------------------
-
---
--- Table structure for table `expense`
---
 
 CREATE TABLE `expense` (
   `id` int(11) NOT NULL,
@@ -52,14 +26,8 @@ CREATE TABLE `expense` (
   `date` timestamp NOT NULL DEFAULT current_timestamp(),
   `description` text DEFAULT NULL,
   `wallet_type` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `savings_goals`
---
-
+) 
+  
 CREATE TABLE `savings_goals` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -67,14 +35,8 @@ CREATE TABLE `savings_goals` (
   `goal_amount` decimal(10,2) NOT NULL,
   `current_savings` decimal(10,2) DEFAULT 0.00,
   `status` varchar(20) DEFAULT 'ongoing'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transactions`
---
-
+) 
+  
 CREATE TABLE `transactions` (
   `transaction_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
@@ -82,28 +44,16 @@ CREATE TABLE `transactions` (
   `account_type` varchar(20) DEFAULT NULL,
   `amount` double DEFAULT NULL,
   `transaction_date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_history`
---
-
+)
+  
 CREATE TABLE `transaction_history` (
   `transaction_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `account_type` varchar(10) DEFAULT NULL,
   `amount` decimal(10,2) DEFAULT NULL,
   `transaction_time` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+) 
+  
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -115,128 +65,62 @@ CREATE TABLE `users` (
   `profile_picture` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `debts`
---
 ALTER TABLE `debts`
   ADD PRIMARY KEY (`debt_id`),
   ADD KEY `fk_user_id` (`user_id`);
 
---
--- Indexes for table `expense`
---
 ALTER TABLE `expense`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `savings_goals`
---
 ALTER TABLE `savings_goals`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `transactions`
---
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`transaction_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `transaction_history`
---
 ALTER TABLE `transaction_history`
   ADD PRIMARY KEY (`transaction_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `debts`
---
 ALTER TABLE `debts`
   MODIFY `debt_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `expense`
---
 ALTER TABLE `expense`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `savings_goals`
---
 ALTER TABLE `savings_goals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `transactions`
---
 ALTER TABLE `transactions`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `transaction_history`
---
 ALTER TABLE `transaction_history`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `debts`
---
 ALTER TABLE `debts`
   ADD CONSTRAINT `debts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `expense`
---
 ALTER TABLE `expense`
   ADD CONSTRAINT `expense_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `savings_goals`
---
 ALTER TABLE `savings_goals`
   ADD CONSTRAINT `savings_goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `transactions`
---
 ALTER TABLE `transactions`
   ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `transaction_history`
---
 ALTER TABLE `transaction_history`
   ADD CONSTRAINT `transaction_history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
